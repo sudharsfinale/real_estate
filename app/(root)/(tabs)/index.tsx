1abbc46f20283ef86d36f5dfff03d4cb2a8e4a3b
sudharsfinale <sudharsfinale@gmail.com>
2025-06-01 11:43:53 +0530

    Setup tailwind and initial setup

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-semibold text-teal-800">Hello World</Text>
+      <Link href={"/sign_in"}>Sign In</Link>
+      <Link href={"/explore"}>Explore</Link>
+      <Link href={"/profile"}>Property</Link>
+      <Link href={"/properties/1"}>Go to Property 1</Link>
+    </View>
+  );
+}
